6de51cb39f364bede068bc6da5a4efde34bacea6
tsotneforester <tsotne.meladze.usa@gmail.com>
2022-06-07 20:46:09 +0400

    stuff added

---
+++ 99-examples/pages/checkbox.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+Select your personality attributes:<br />
+<input type="checkbox" name="attributes[]" value="perky" /> Perky<br />
+<input type="checkbox" name="attributes[]" value="morose" /> Morose<br />
+<input type="checkbox" name="attributes[]" value="thinking" />
+Thinking<br />
+<input type="checkbox" name="attributes[]" value="feeling" /> Feeling<br
+/>
+<input type="checkbox" name="attributes[]" value="thrifty" />Spendthrift<br />
+<input type="checkbox" name="attributes[]" value="shopper" /> Shopper<br
+/>
+<br />
+<input type="submit" name="s" value="Record my personality!" />
+</form>
+<?php
+
+if (isset($_POST['s'])) {
+   $description = join (' ', $_POST['attributes']);
+   echo "You have a {$description} personality.";
+}
+      ?>
+</body>
+</html>
---
+++ 99-examples/pages/dynamic/pages/Comments.php
@@ -0,0 +1,3 @@
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum laoreet varius. Integer venenatis augue dapibus efficitur aliquet. Quisque viverra nisl semper, consectetur erat mattis, pellentesque libero. Etiam est odio, facilisis sed enim ac, iaculis luctus neque. Fusce lacus leo, fermentum et cursus elementum, cursus et turpis. Suspendisse sit amet odio elementum nunc varius mollis et et massa. Donec gravida auctor lacus eu rutrum. Duis venenatis gravida justo, eget mollis sapien pulvinar et. Fusce auctor quis augue vel interdum. Mauris eu massa vel mauris mollis porttitor. Donec aliquam mauris odio, vitae tristique ante suscipit sit amet. Sed sed erat metus. Ut pretium ultrices magna eget maximus.
+		<h1>Comments</h1>
